8e513a11a85156cc0e599c5853796b2f9e22f60a
Prince Gildas <princegildas29@gmail.com>
2024-03-01 22:29:29 +0000

    mis à jour des fichiers config pour forcer les logs
    mis à jour des fichiers config pour forcer les logs

--- apps/config.py
+++ apps/config.py
@@ -7,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.exc import OperationalError
 import sqlite3
+import logging
 
 class Email_config(object):
     MAIL_USERNAME = 'vocasaracontactapp@gmail.com'
@@ -106,6 +107,9 @@ class Config(object):
 class ProductionConfig(Config):
 
     DEBUG = False
+        # Configurer le logging pour enregistrer les erreurs dans un fichier
+    logging.basicConfig(filename='log/app.log', level=logging.ERROR,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
 
     # Security
     SESSION_COOKIE_HTTPONLY = True
--- gunicorn-cfg.py
+++ gunicorn-cfg.py
@@ -1,11 +1,8 @@
 # -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
 
 bind = '0.0.0.0:5005'
 workers = 1
-accesslog = '-'
+accesslog = 'log/access.log'
 loglevel = 'debug'
 capture_output = True
 enable_stdio_inheritance = True
